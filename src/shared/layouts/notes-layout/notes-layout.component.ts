37b5fb13253ed950307a08adbf49c42a39d92540
Leidi <leididelapuente3@gmail.com>
2025-07-22 17:52:39 -0500

    feat(notes-layout): integrate sidebar component into notes layout

--- src/shared/layouts/notes-layout/notes-layout.component.ts
+++ src/shared/layouts/notes-layout/notes-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from 'src/shared/components/layout/header/header.component';
+import { SidebarComponent } from 'src/shared/components/layout/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-notes-layout',
-  imports: [HeaderComponent, RouterOutlet],
+  imports: [HeaderComponent, RouterOutlet, SidebarComponent],
   templateUrl: './notes-layout.component.html',
 })
 export class NotesLayoutComponent {}
