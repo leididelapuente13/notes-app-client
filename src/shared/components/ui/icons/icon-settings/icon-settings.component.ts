5348cbdffc39a20ba0da8b920614e36cdbe347f3
Leidi <leididelapuente3@gmail.com>
2025-07-22 17:43:20 -0500

    feat(icon-settings.component.html): Add icon setting
    Remove the component settings-icon and create it with the right naming structure.

---
+++ src/shared/components/ui/icons/icon-settings/icon-settings.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-settings',
+  imports: [],
+  templateUrl: './icon-settings.component.html',
+})
+export class IconSettingsComponent {}
