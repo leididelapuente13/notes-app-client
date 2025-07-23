c165f4e3348187dde540a2ec7e8b023ff73c1183
Leidi <leididelapuente3@gmail.com>
2025-07-23 17:27:41 -0500

    feat(icons): add icon-search and icon-tag components with SVG templates

---
+++ src/shared/components/ui/icons/icon-search/icon-search.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-search',
+  imports: [],
+  templateUrl: './icon-search.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class IconSearchComponent {}
---
+++ src/shared/components/ui/icons/icon-tag/icon-tag.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-tag',
+  imports: [],
+  templateUrl: './icon-tag.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class IconTagComponent {}
