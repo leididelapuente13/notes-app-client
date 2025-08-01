06bf6692f9aa789dadcfca850cf00e4b20d07fba
Leidi <leididelapuente3@gmail.com>
2025-08-01 10:46:56 -0500

    feat(settings): implement sidebar navigation with links and logout button

--- src/features/settings/components/layout/settings-sidebar/settings-sidebar.component.ts
+++ src/features/settings/components/layout/settings-sidebar/settings-sidebar.component.ts
@@ -1,9 +1,35 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { LogoutButtonComponent } from '@features/auth/components';
+import {
+  IconFontComponent,
+  IconLockComponent,
+  IconSunComponent,
+  NavItemComponent,
+} from '@shared/components';
+import { Link } from '@shared/interfaces';
 
 @Component({
   selector: 'settings-sidebar',
-  imports: [],
+  imports: [NavItemComponent, LogoutButtonComponent],
   templateUrl: './settings-sidebar.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SettingsSidebarComponent {}
+export class SettingsSidebarComponent {
+  links: Link[] = [
+    {
+      label: 'Color Theme',
+      path: '/settings/theme',
+      icon: IconSunComponent,
+    },
+    {
+      label: 'Font Theme',
+      path: '/settings/font',
+      icon: IconFontComponent,
+    },
+    {
+      label: 'Change Password',
+      path: '/settings/change-password',
+      icon: IconLockComponent,
+    },
+  ];
+}
