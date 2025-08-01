91207665c54bb5c111fc30a1accc3b31f89b4e3d
Leidi <leididelapuente3@gmail.com>
2025-08-01 08:33:35 -0500

    style(settings): format import statements for better readability

--- src/shared/layouts/settings-layout/settings-layout.component.ts
+++ src/shared/layouts/settings-layout/settings-layout.component.ts
@@ -1,11 +1,20 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BottomNavigationComponent, HeaderComponent, SidebarComponent } from "@shared/components";
+import {
+  BottomNavigationComponent,
+  HeaderComponent,
+  SidebarComponent,
+} from '@shared/components';
 
 @Component({
   selector: 'app-settings-layout',
-  imports: [BottomNavigationComponent, RouterOutlet, HeaderComponent, SidebarComponent],
+  imports: [
+    BottomNavigationComponent,
+    RouterOutlet,
+    HeaderComponent,
+    SidebarComponent,
+  ],
   templateUrl: './settings-layout.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SettingsLayoutComponent { }
+export class SettingsLayoutComponent {}
