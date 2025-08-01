6a45124d1d28dbe7e19ea2432a54b4871505df71
Leidi <leididelapuente3@gmail.com>
2025-08-01 07:47:11 -0500

    fix(header): adjust button structure for settings icon and add navigation to settings

--- src/shared/components/layout/header/header.component.ts
+++ src/shared/components/layout/header/header.component.ts
@@ -2,10 +2,16 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { SectionTitleComponent } from '../../ui/title/title.component';
 import { LogoComponent } from '../../ui/logo/logo.component';
 import { IconSettingsComponent } from '../../ui/icons/icon-settings/icon-settings.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [SectionTitleComponent, LogoComponent, IconSettingsComponent],
+  imports: [
+    RouterLink,
+    SectionTitleComponent,
+    LogoComponent,
+    IconSettingsComponent,
+  ],
   templateUrl: './header.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
