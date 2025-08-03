4a240c6b8bcd2751f69c5773188b699ae5a16068
Leidi De La Puente <leididelapuente3@gmail.com>
2025-08-02 23:49:44 -0500

    refactor(nav-item): streamline padding class logic and remove redundant conditions

--- src/shared/components/layout/sidebar/nav/nav-item/nav-item.component.ts
+++ src/shared/components/layout/sidebar/nav/nav-item/nav-item.component.ts
@@ -1,5 +1,5 @@
 import { NgComponentOutlet } from '@angular/common';
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { IconArrowRightComponent } from '@shared/components/ui/icons';
 import { Link } from '@shared/interfaces';
@@ -17,4 +17,10 @@ import { Link } from '@shared/interfaces';
 export class NavItemComponent {
   item = input.required<Link>();
   paddingSize = input.required<'sm' | 'md'>();
+  protected readonly paddingClass = computed(() => {
+    if(this.paddingSize() === 'md'){
+      return 'py-2.5 px-3'
+    }
+    return 'p-2'
+  });
 }
