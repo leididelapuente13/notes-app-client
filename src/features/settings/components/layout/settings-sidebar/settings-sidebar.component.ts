7717c6e9b3747b34189acdcbca9c271919ace223
Leidi De La Puente <leididelapuente3@gmail.com>
2025-08-02 22:58:24 -0500

    feat(settings-sidebar): refactor sidebar classes and improve responsiveness

--- src/features/settings/components/layout/settings-sidebar/settings-sidebar.component.ts
+++ src/features/settings/components/layout/settings-sidebar/settings-sidebar.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  input,
+} from '@angular/core';
 import { LogoutButtonComponent } from '@features/auth/components';
 import {
   IconFontComponent,
@@ -15,6 +20,19 @@ import { Link } from '@shared/interfaces';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SettingsSidebarComponent {
+  renderInFullScreen = input.required<boolean>();
+
+  protected readonly sidebarClasses = computed(() => {
+    const baseClasses =
+      'w-screen lg:w-[258px] h-full border-solid border-r border-neutral-200 dark:border-neutral-200/30 overflow-y-auto';
+
+    if (this.renderInFullScreen()) {
+      return `block px-4 py-6 md:px-8 ${baseClasses}`;
+    }
+
+    return `hidden lg:block ${baseClasses} py-5 pl-[32px] pr-[16px]`;
+  });
+
   links: Link[] = [
     {
       label: 'Color Theme',
