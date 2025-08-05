ed9175912cc6600b984e276ccc16f43b36869fd4
Leidi <leididelapuente3@gmail.com>
2025-08-05 14:28:30 -0500

    feat(button): implement button component with dynamic classes and styles

--- src/shared/components/ui/button/button.component.ts
+++ src/shared/components/ui/button/button.component.ts
@@ -1,16 +1,44 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { NgComponentOutlet } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, input, Type } from '@angular/core';
 
 @Component({
   selector: 'app-button',
-  imports: [],
+  imports: [NgComponentOutlet],
   templateUrl: './button.component.html',
+  styleUrl: './button.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ButtonComponent {
+  icon = input<Type<unknown>>();
   label = input.required<string>();
+  variant = input.required<'primary' | 'secondary' | 'border'>();
+  isDangerButton = input<boolean>(false);
+  alignContent = input<'left' | 'center'>('center');
+  disabled = input<boolean>(false);
   onClick = input.required<() => void>();
 
+  activeClasses = computed(() => {
+    switch (this.variant()) {
+      case 'primary':
+        return 'btn-primary';
+      case 'secondary':
+        return 'btn-secondary';
+      case 'border':
+        return 'btn-border';
+      default:
+        return;
+    }
+  });
+
+  disabledClasses = computed(()=>{
+    if(this.disabled() && this.variant() === 'primary'){
+      return 'btn-primary-disabled'
+    }
+    return 'btn-disabled'
+  })
+
   clickHandler() {
+    if(this.disabled()) return
     const click = this.onClick();
     click();
   }
