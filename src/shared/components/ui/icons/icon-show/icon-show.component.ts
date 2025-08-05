9e61fb26d4192a1898abf52ff073e38752129905
Leidi <leididelapuente3@gmail.com>
2025-08-05 17:28:40 -0500

    feat(icon-show): add icon-show component with SVG template

---
+++ src/shared/components/ui/icons/icon-show/icon-show.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-show',
+  imports: [],
+  templateUrl: './icon-show.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class IconShowComponent {}
