4e9ae4cb7a0e2b3ae5746ae4ec0ec94254c3f338
Leidi De La Puente <leididelapuente3@gmail.com>
2025-08-08 22:20:50 -0500

    refactor(modal): update modal configuration to use ModalVariants instead of icons

--- src/shared/components/ui/modal/modal.component.ts
+++ src/shared/components/ui/modal/modal.component.ts
@@ -7,7 +7,7 @@ import {
   input,
 } from '@angular/core';
 import { IconArchiveComponent, IconDeleteComponent } from '../icons';
-import { icons, ModalConfig } from '@shared/interfaces';
+import { ModalConfig, ModalVariants } from '@shared/interfaces';
 import { ButtonComponent } from '../button/button.component';
 import { ModalService } from '@core/services/modal.service';
 
@@ -21,12 +21,10 @@ export class ModalComponent {
   private readonly modalService = inject(ModalService);
 
   readonly visible = computed(() => this.modalService.isVisible());
-  readonly title = input.required<string>();
-  readonly description = input.required<string>();
-  readonly iconName = input.required<icons>();
+  readonly variant = input.required<ModalVariants>();
   readonly onConfirm = input.required<() => void>();
 
-  private readonly _modalConfigMap: Record<icons, ModalConfig> = {
+  private readonly _modalConfigMap: Record<ModalVariants, ModalConfig> = {
     archive: {
       icon: IconArchiveComponent,
       buttonLabel: 'Archive Note',
@@ -46,7 +44,7 @@ export class ModalComponent {
   };
 
   private readonly modalConfigMap = computed(
-    () => this._modalConfigMap[this.iconName()],
+    () => this._modalConfigMap[this.variant()],
   );
 
   protected readonly iconComponent = computed(() => this.modalConfigMap().icon);
--- src/shared/interfaces/ui/modal.interface.ts
+++ src/shared/interfaces/ui/modal.interface.ts
@@ -1,6 +1,6 @@
 import { Type } from '@angular/core';
 
-export type icons = 'archive' | 'delete';
+export type ModalVariants = 'archive' | 'delete';
 export interface ModalConfig {
   icon: Type<unknown>;
   buttonLabel: string;
