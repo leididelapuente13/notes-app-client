44253a4f65efbb808e8a586ee802e06db0a99007
Leidi De La Puente <leididelapuente3@gmail.com>
2025-08-08 22:13:41 -0500

    feat(modal): add ModalConfig interface to define modal properties

--- src/shared/interfaces/ui/modal.interface.ts
+++ src/shared/interfaces/ui/modal.interface.ts
@@ -1,1 +1,10 @@
+import { Type } from '@angular/core';
+
 export type icons = 'archive' | 'delete';
+export interface ModalConfig {
+  icon: Type<unknown>;
+  buttonLabel: string;
+  variant: 'primary' | 'danger';
+  title: string;
+  description: string;
+}
