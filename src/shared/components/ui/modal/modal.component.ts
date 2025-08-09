6b3ba8ed5c01dc2fff50feeb4565bf7298387d53
Leidi De La Puente <leididelapuente3@gmail.com>
2025-08-08 22:15:33 -0500

    feat(modal): refactor modal component to use modalConfigMap for dynamic content

--- src/shared/components/ui/modal/modal.component.ts
+++ src/shared/components/ui/modal/modal.component.ts
@@ -5,10 +5,9 @@ import {
   computed,
   inject,
   input,
-  Type,
 } from '@angular/core';
-import { IconArchiveComponent } from '../icons';
-import { icons } from '@shared/interfaces';
+import { IconArchiveComponent, IconDeleteComponent } from '../icons';
+import { icons, ModalConfig } from '@shared/interfaces';
 import { ButtonComponent } from '../button/button.component';
 import { ModalService } from '@core/services/modal.service';
 
@@ -20,18 +19,51 @@ import { ModalService } from '@core/services/modal.service';
 })
 export class ModalComponent {
   private readonly modalService = inject(ModalService);
+
   readonly visible = computed(() => this.modalService.isVisible());
   readonly title = input.required<string>();
   readonly description = input.required<string>();
   readonly iconName = input.required<icons>();
-  private readonly iconMap = new Map<string, Type<unknown>>([
-    ['archive', IconArchiveComponent],
-    ['delete', IconArchiveComponent],
-  ]);
+  readonly onConfirm = input.required<() => void>();
 
-  protected getIcon(key: icons): Type<unknown> | null {
-    return this.iconMap.get(key) ?? null;
-  }
+  private readonly _modalConfigMap: Record<icons, ModalConfig> = {
+    archive: {
+      icon: IconArchiveComponent,
+      buttonLabel: 'Archive Note',
+      variant: 'primary',
+      title: 'Archive Note',
+      description:
+        'Are you sure you want to archive this note? You can find it in the Archived Notes section and restore it anytime.',
+    },
+    delete: {
+      icon: IconDeleteComponent,
+      buttonLabel: 'Delete Note',
+      variant: 'danger',
+      title: 'Delete Note',
+      description:
+        'Are you sure you want to permanently delete this note? This action cannot be undone.',
+    },
+  };
+
+  private readonly modalConfigMap = computed(
+    () => this._modalConfigMap[this.iconName()],
+  );
+
+  protected readonly iconComponent = computed(() => this.modalConfigMap().icon);
+
+  protected readonly buttonLabel = computed(
+    () => this.modalConfigMap().buttonLabel,
+  );
+
+  protected readonly buttonVariant = computed(
+    () => this.modalConfigMap().variant,
+  );
+
+  protected readonly modalTitle = computed(() => this.modalConfigMap().title);
+
+  protected readonly modalDescription = computed(
+    () => this.modalConfigMap().description,
+  );
 
   protected onCancel = () => {
     this.modalService.toggleModal();
