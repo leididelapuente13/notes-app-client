3920a935a2e0c70bd05d13ba19995efddaa5602e
Leidi De La Puente <leididelapuente3@gmail.com>
2025-08-17 01:03:41 -0500

    refactor(message): remove unused comments from message component

--- src/shared/components/feedback/messages/message/message.component.ts
+++ src/shared/components/feedback/messages/message/message.component.ts
@@ -10,10 +10,3 @@ export class MessageComponent {
   readonly variant = input.required<'archived' | 'search' | 'tag'>();
   readonly match = input<string>('');
 }
-/**
- * All your archived notes are stored here. You can restore or delete them anytime.
- *
- * All notes matching ”Dev” are displayed below.
- *
- * All notes matching ”Bananas” are displayed below.
- */
