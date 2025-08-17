c77998461e2c7d0b4fc55e4bdc2dbb65b87cfc2d
Leidi De La Puente <leididelapuente3@gmail.com>
2025-08-17 00:51:50 -0500

    refactor(empty-state): remove unused comments from empty state component

--- src/shared/components/feedback/messages/empty-state/empty-state.component.ts
+++ src/shared/components/feedback/messages/empty-state/empty-state.component.ts
@@ -10,11 +10,3 @@ import { RouterLink } from '@angular/router';
 export class EmptyStateComponent {
   variant = input.required<'search' | 'archived' | 'all-notes'>();
 }
-
-/**
- * All your archived notes are stored here. You can restore or delete them anytime.
- *
- * All notes matching ”Dev” are displayed below.
- *
- * All notes matching ”Bananas” are displayed below.
- */
