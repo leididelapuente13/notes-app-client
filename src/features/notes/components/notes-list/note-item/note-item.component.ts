d9ad288ecdc6a43153df060163260d1ef8f1adca
Leidi De La Puente <leididelapuente3@gmail.com>
2025-08-18 16:27:55 -0500

    refactor(note-item): remove unnecessary constructor and simplify isSelected logic

--- src/features/notes/components/notes-list/note-item/note-item.component.ts
+++ src/features/notes/components/notes-list/note-item/note-item.component.ts
@@ -4,7 +4,8 @@ import {
   Component,
   inject,
   input,
-  signal,
+  signal, OnInit,
+  computed,
 } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { Note } from '@features/notes/interfaces/Note.interface';
@@ -15,22 +16,18 @@ import { Note } from '@features/notes/interfaces/Note.interface';
   templateUrl: './note-item.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NoteItemComponent {
+export class NoteItemComponent implements OnInit {
   private readonly route = inject(ActivatedRoute);
 
   readonly note = input.required<Note>();
   protected readonly selectedNote = signal<string | null>(null);
-
-  constructor() {
+  protected readonly isSelected = computed(() => {
+    if (!this.selectedNote) return false;
+    return this.selectedNote() === this.note().id;
+  })
+  ngOnInit() {
     this.route.paramMap.subscribe((params) => {
       this.selectedNote.set(params.get('id'));
     });
-  }
-
-  isSelected(): boolean {
-    console.log(this.selectedNote);
-    if (!this.selectedNote) return false;
-    console.log('item', this.selectedNote() === this.note().id);
-    return this.selectedNote() === this.note().id;
   }
 }
