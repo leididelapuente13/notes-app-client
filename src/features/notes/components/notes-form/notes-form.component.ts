ba5e0c8775fea96919cdfcfd5b203144d10a1c77
Leidi <leididelapuente3@gmail.com>
2025-08-22 16:19:27 -0500

    feat(notes-form): add notes-form component
    add notes form component with title, tags, and edited handling

---
+++ src/features/notes/components/notes-form/notes-form.component.ts
@@ -0,0 +1,43 @@
+import { DatePipe } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  signal,
+} from '@angular/core';
+import { Note } from '@features/notes/interfaces/Note.interface';
+import {
+  IconTagComponent,
+  IconClockComponent,
+  ButtonComponent,
+} from '@shared/components';
+import { HeaderControlComponent } from '../header-control/header-control.component';
+
+@Component({
+  selector: 'notes-form',
+  imports: [
+    DatePipe,
+    HeaderControlComponent,
+    IconTagComponent,
+    IconClockComponent,
+    ButtonComponent,
+  ],
+  templateUrl: './notes-form.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NotesFormComponent {
+  readonly noteId = input<string>();
+  readonly note = signal<Note>({
+    id: '2',
+    title: 'Angular Signals',
+    tags: ['angular', 'signals'],
+    updatedAt: new Date('2024-06-02T09:30:00Z'),
+    content: 'Exploring the new signals API in Angular 20.',
+    archived: false,
+  });
+
+  protected getNoteData() {
+    if (!this.noteId()) return;
+    console.log(this.noteId());
+  }
+}
