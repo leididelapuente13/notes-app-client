330449bbd9b7c216da9d1059d83bdb2fe731177d
Leidi <leididelapuente3@gmail.com>
2025-08-25 10:32:50 -0500

    feat(title-component): enhance title display with conditional matching

--- src/shared/components/ui/title/title.component.ts
+++ src/shared/components/ui/title/title.component.ts
@@ -1,11 +1,13 @@
+import { TitleCasePipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-title',
-  imports: [],
+  imports: [TitleCasePipe],
   templateUrl: './title.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SectionTitleComponent {
   title = input.required<string>();
+  match = input<string | undefined>(undefined);
 }
