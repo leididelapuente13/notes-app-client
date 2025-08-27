2968f34d7f0e5e030448355fe3aaee87e75548e2
Leidi <leididelapuente3@gmail.com>
2025-08-27 17:37:59 -0500

    feat(notes-form-page): add page to create notes

---
+++ src/features/notes/components/add-note-button/add-note-button.component.ts
@@ -0,0 +1,31 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { IconPlusComponent } from '@shared/components';
+import { filter } from 'rxjs';
+
+@Component({
+  selector: 'notes-add-note-button',
+  imports: [IconPlusComponent, RouterLink],
+  templateUrl: './add-note-button.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AddNoteButtonComponent implements OnInit {
+  private readonly router = inject(Router);
+
+  protected readonly isFormPage = signal<boolean>(false);
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.isFormPage.set(event.url.includes('/notes/form'));
+      });
+    this.isFormPage.set(false);
+  }
+}
