9fbcaf5b4611825ad4dec9f7bf7e9027d4cea68c
Leidi <leididelapuente3@gmail.com>
2025-09-01 11:58:26 -0500

    feat(notes): add index exports for components and interfaces

---
+++ src/features/notes/components/index.ts
@@ -0,0 +1,7 @@
+export * from './add-note-button/add-note-button.component';
+export * from './note-details/note-details.component';
+export * from './notes-form/notes-form.component';
+export * from './notes-list/notes-list.component';
+export * from './right-menu/right-menu.component';
+export * from './sidebar/sidebar.component';
+export * from './tags-list/tags-list.component';
