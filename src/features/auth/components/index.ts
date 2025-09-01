7c5a0001fc6f1108ec5202f03c061cfb0a67a76b
Leidi <leididelapuente3@gmail.com>
2025-09-01 11:34:07 -0500

    feat(update-password): add UpdatePasswordComponent with template

--- src/features/auth/components/index.ts
+++ src/features/auth/components/index.ts
@@ -1,1 +1,2 @@
 export * from './ui/logout-button/logout-button.component';
+export * from './update-password/update-password.component';
---
+++ src/features/auth/components/update-password/update-password.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-password',
+  imports: [],
+  templateUrl: './update-password.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class UpdatePasswordComponent {}
