d3e22815b0de26ff18fc3751a463efd10685471f
Leidi <leididelapuente3@gmail.com>
2025-09-01 11:56:05 -0500

    refactor(auth): remove UpdatePasswordComponent and its template from the project

--- src/features/auth/components/index.ts
+++ src/features/auth/components/index.ts
@@ -1,2 +1,1 @@
 export * from './ui/logout-button/logout-button.component';
-export * from './update-password/update-password.component';
--- src/features/auth/components/update-password/update-password.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-update-password',
-  imports: [],
-  templateUrl: './update-password.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class UpdatePasswordComponent {}
