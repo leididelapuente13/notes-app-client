700ece02992369423e57f58b14ec0f37f2c2ea68
Leidi <leididelapuente3@gmail.com>
2025-09-04 17:36:03 -0500

    style(button): apply text preset class to label for consistency

---
+++ src/shared/components/ui/icons/icon-google/icon-google.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-google',
+  imports: [],
+  templateUrl: './icon-google.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class IconGoogleComponent {}
